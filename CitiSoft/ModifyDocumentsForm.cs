d8a40caa0b2acd436094afe7949b0207434d570f
Koshkxrov <koshkarov.uk@gmail.com>
2023-11-29 14:39:02 +0000

    file dropbox created

--- CitiSoft/ModifyDocumentsForm.cs
+++ CitiSoft/ModifyDocumentsForm.cs
@@ -35,6 +35,47 @@ namespace CitiSoft
                 e.Effect = DragDropEffects.Copy;
         }
 
-       
+        void fileDropPBox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (vendorIDTxtBox.Text == "")
+            {
+                MessageBox.Show("Please provide vendor ID first");
+                return;
+            }
+            if (!InputValidation.CheckValueExists(DataBaseManager.citiSoftDatabaseConnectionString, "VendorInfo", "vid", vendorIDTxtBox.Text))
+            {
+                MessageBox.Show("Provided vendor ID does not exist");
+                return;
+            }
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var file in files)
+            {
+                try
+                {
+                    byte[] fileData = File.ReadAllBytes(file);
+
+                    using (SqlConnection connection = new SqlConnection(DataBaseManager.citiSoftDatabaseConnectionString))
+                    {
+                        //string query = "INSERT INTO VendorInfo (docAttach) VALUES (@Data) WHERE vid = @VendorID;";
+                        string query = "UPDATE VendorInfo SET docAttach = @Data WHERE vid = @VendorID";
+
+
+                        using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.Add("@Data", SqlDbType.VarBinary, -1).Value = fileData;
+                            command.Parameters.AddWithValue("@VendorID", vendorIDTxtBox.Text);
+
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    MessageBox.Show("File was successfully added");
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Error uploading file" + exc);
+                }
+            }
+        }
     }
 }
