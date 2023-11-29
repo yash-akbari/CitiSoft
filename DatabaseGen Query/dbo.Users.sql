358c68eb0fade3d6dfae447e0d90e71bdd0f9147
Koshkxrov <koshkarov.uk@gmail.com>
2023-11-29 20:22:37 +0000

    user database changed

---
+++ DatabaseGen Query/dbo.Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[User] (
+    [uid]   INT           IDENTITY (1, 1) NOT NULL,
+    [fn]    NVARCHAR (30) NOT NULL,
+    [ln]    NVARCHAR (30) NOT NULL,
+    [phone] NVARCHAR (15) NOT NULL,
+    [email] NVARCHAR (50) NOT NULL,
+    [pwd]   NVARCHAR (30) NOT NULL,
+    [uType] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([uid] ASC)
+);
+
